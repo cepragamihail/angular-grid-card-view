dec69f0affd9ff135db79f44cb4d00a8971a0a91
cepragamihail <yahim09@gmail.com>
2020-10-17 18:24:28 +0300

    initial view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatCardModule } from "@angular/material/card";
@@ -21,7 +20,7 @@ import { FlexLayoutModule } from "@angular/flex-layout";
     MatButtonModule,
     FlexLayoutModule
   ],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
